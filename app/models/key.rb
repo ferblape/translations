20c3efadbda544a31b22f3ee779f0da46a92a1af
Fernando Blat <ferblape@gmail.com>
2011-10-23 11:47:05 +0200

    Import from a locale file and create all the keys and the reference translations

--- app/models/key.rb
+++ app/models/key.rb
@@ -22,5 +22,60 @@ class Key < ActiveRecord::Base
 
   end
 
+  # Valid options:
+  #  - locale: the locale in which import the keys (optional)
+  #  - file_path: the path to the YML file (required)
+  def self.import(options = {})
+    default_options = {
+      :locale => Rails.configuration.main_locale,
+    }
+    options = default_options.merge(options)
+
+    locales = YAML.load(File.read(options[:file_path]))
+    raise "Locale in file and main_locale are different" if options[:locale].to_sym != locales.keys.first.to_sym
+
+    result = get_keys(locales.values.first, [], [])
+
+    existing_keys = Key.all.map(&:key).sort
+    new_keys = result.map{ |h| h[:key] }
+
+    # Old keys, which does not exist in the new locale file
+    # must be removed becase are not used anymore
+    existing_keys - new_keys.each do |key|
+      Key.find_by_key(key).try(:destroy)
+    end
+
+    result.each do |h|
+      # If key does not exists, a new key is created
+      unless key = Key.find_by_key(h[:key])
+        key = Key.create :key => h[:key]
+      end
+      if existing_keys.include?(h[:key])
+        translation = key.translations.where(:language => options[:locale]).first
+      end
+      if translation
+        translation.update_attribute(:translation, h[:value])
+      else
+        translation = key.translations.new :language => options[:locale], :translation => h[:value]
+        translation.save!
+      end
+    end
+  end
+
+  private
+
+  def self.get_keys(locales, keys, result)
+    result = []
+    value = nil
+    if locales.is_a?(Hash)
+      locales.keys.each do |key|
+        r = get_keys(locales[key], keys + [key], result)
+        result << r
+      end
+      result.flatten
+    else
+      {:key => keys.join('.'), :value => locales}
+    end
+  end
 
 end
--- app/models/translation.rb
+++ app/models/translation.rb
@@ -4,19 +4,24 @@ class Translation < ActiveRecord::Base
   belongs_to :user
   has_many :votes, :dependent => :destroy
   # scope :order_by_votes, order("#{translations}.votes_count DESC")
-  
+
   validates :language, :presence => true
   validates :translation, :presence => true
   validates :language, :inclusion => { :in => %w{en es}}
-  
+
   after_create :create_or_update_vote
- 
+
   def translation=(value)
     write_attribute(:translation, value.strip)
   end
-  
+
+  def language=(value)
+    value = value.to_s
+    super
+  end
+
   def save_if_not_exist
-    if t = Translation.where(:translation => translation).first
+    if t = Translation.where(:translation => translation, :language => self.language).first
       if vote = t.key.votes.where(:user_id => user_id).first
          vote.update_attribute(:translation, t)
       else
@@ -26,26 +31,24 @@ class Translation < ActiveRecord::Base
       save
     end
   end
-  
+
   def next
     Key.where("id < #{self.key_id}").order('id DESC').first
   end
-  
+
   def previous
     Key.where("id > #{self.key_id}").order('id ASC').first
   end
 
 
   private
+
   def create_or_update_vote
     if vote = key.votes.where(:user_id => user_id).first
        vote.update_attribute(:translation, self)
-    else 
+    else
       votes.create(:user => user)
-    end   
+    end
   end
-  
-
-
 
 end
